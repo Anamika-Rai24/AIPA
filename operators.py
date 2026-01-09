d927402113547e184a54b4daca13735f3db84825
Anamika-Rai24 <anamikarai24072006@gmail.com>
2026-01-09 15:07:23 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,72 @@
+# Arithmetic Operators
+x = 15
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y) # floot value 
+print(x // y) # integer value (Floor division)
+print(x % y) # reminder value (Modulo)
+print(x ** y) # Exponentiation (Power of the number)
+
+
+# Comparison operators
+x = 30
+y = 26
+print(x > y)
+print(x < y)
+print(x == y)
+print(x >= y)
+print(x != y)
+print(x <= y)
+
+# Logical operators
+a = 12
+b = 15
+c = 17
+print(a<b and c>b)
+print(a<b or c>a)
+print( not a>b )
+
+# Logical operators
+x = True
+y = False
+print(x and y) # Returns true both conditions must be true(return false if at least one condition is false.)
+print(x or y)  # Returns true if at least one condition is true(Returns false only if both conditions are false.)
+print( not x ) # reverse
+
+# Assigment Operators
+p = 10
+q = 5
+p+=q
+print(p)
+p-=q
+print(p)
+p*=q
+print(p)
+p/=q
+print(p)
+p%=q
+print(p)
+p**=q
+print(p)
+p//=q
+print(p)
+
+# Assigment Operators
+p = 30
+q = 15
+p+=q
+print(p)
+p-=q
+print(p)
+p*=q
+print(p)
+p/=q
+print(p)
+p%=q
+print(p)
+p**=q
+print(p)
+p//=q
+print(p)
