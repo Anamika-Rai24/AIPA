13880ad1ef1298c675a1f5e097d10efc0d41ace7
Anamika-Rai24 <anamikarai24072006@gmail.com>
2026-01-09 15:08:09 +0530

    Add files via upload

---
+++ Pybasics.py
@@ -0,0 +1,28 @@
+print("Hello, Python Learners!")
+name= 'Anu'
+age= 18
+print(f"My name is {name} and I am {age} year old.")
+
+a=10
+b=5
+print("The Sum of 10 & 5 :", 10+5)
+print("The Multiplication of 10 & 5 :", 10*5)
+print("The Division of 10 & 5 :",10-5 )
+print("The Subtraction of 10 & 5 :", 10/5)
+
+x=5
+y=6
+c=x+y
+print("The Sum of x & y is :",x+y )
+print("The Sum of",x,"and",y,"is :",c)#normal print statement
+print(f"The Sum of {x} and {y} is: {c}")#f string
+
+print("""
+       Python is fun!
+       Let's learn it step by step.
+       Keep practicing every day.""") #multi line print
+
+x= 23
+
+print(type(x))
+
