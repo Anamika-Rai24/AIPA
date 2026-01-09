95360ba5ade5543793a336b05ba191990e38d7d7
Anamika-Rai24 <anamikarai24072006@gmail.com>
2026-01-09 15:07:46 +0530

    Add files via upload

---
+++ print1.py
@@ -0,0 +1,26 @@
+print ("Welcome to NSTI")
+print("Hello world")
+
+print("Anu") #in-line print 
+
+
+print("Anamika \n Rai") #next line print
+
+print("""This is my pen
+
+""") #multi line print
+print("The Sum of ")
+
+
+
+#Concatenating string using the '+' operator
+firstname = "Riya"
+lastname = "Singh"
+print("My name is-" +firstname+""+lastname + "!")
+#f-string for formatting output
+age = 20
+print(f"I am {20} year old")
+#Changing the end of the print (no new line)
+print("NSTI,", end="")
+print("Allhabad")
+
