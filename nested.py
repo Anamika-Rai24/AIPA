99c043b6d040ec8e384a8699b9b1a22116e81828
Anamika-Rai24 <anamikarai24072006@gmail.com>
2026-01-22 15:53:58 +0530

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,68 @@
+# age = 25
+# has_driver_license = True
+# has_car = False
+
+# if age >= 18:
+#     print("You are old enough to drive.")
+#     if has_driver_license:
+#         print("You have a driver's license.")
+#         if has_car:
+#             print("You can drive your car!")
+#         else:
+#             print("You have a license but no car.")
+#     else:
+#         print("You are old enough but don't have a driver's license.")
+# else:
+#     print("You are too young to drive.")
+
+# Nested if
+college = input("Enter your Institude :")
+if college == "NSTI W":
+    print("Welcome to NSTI W.")
+    trade = input("Enter your trade :")
+    if trade == "COPA":
+        print("Lab no. 13")
+    elif trade == "AIPA":
+        print("Lab no. 15")
+    elif trade == "CSA":
+        print("Lab no. 14")
+    elif trade == "EM":
+        print("Lab no. 12")
+    else:
+        print("Enter valid trade.")
+else:
+    ("Not a student of NSTI W.")                        
+
+
+# age =int(input("Enter your age :"))
+# if age >= 18:
+#     citizen = input("Enter your citizenship :")
+#     if citizen == "Indian":
+#         print("Eligible for voting.")
+#     else:
+#         print("Not Indian citizen.")
+# else:
+#     print("Not eligible for voting.")            
+
+
+# marks = int(input("Enter your marks :"))
+# if marks >= 30:
+#     if marks >= 80:
+#         print("A")
+#     elif marks >= 70:
+#         print("B")
+#     elif marks >= 60:
+#         print("C")
+#     else:
+#         print("D")
+# else:
+#     print("fail")                
+
+
+
+
+
+
+
+
+
